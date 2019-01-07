909b177b7ee7d42a2fdc71b64f540d0d48224e6a
Daniel <ddieppad@live.com>
2019-01-07 07:34:42 -0800

    Update dev_setup.ps1
    added my own stuff

--- personal/dev_setup.ps1
+++ personal/dev_setup.ps1
@@ -12,7 +12,7 @@ $strpos = $helperUri.IndexOf($bstrappackage)
 $helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
 $helperUri = $helperUri.TrimStart("'", " ")
 $helperUri = $helperUri.TrimEnd("'", " ")
-$strpos = $helperUri.LastIndexOf("/demos/")
+$strpos = $helperUri.LastIndexOf("/personal/")
 $helperUri = $helperUri.Substring(0, $strpos)
 $helperUri += "/scripts"
 write-host "helper script base URI is $helperUri"
@@ -36,35 +36,48 @@ executeScript "WSL.ps1";
 RefreshEnv
 executeScript "Docker.ps1";
 
-choco install -y powershell-core
-choco install -y azure-cli
-Install-Module -Force Az
-Install-Module -Force posh-git
-choco install -y microsoftazurestorageexplorer
-choco install -y terraform
-
 # Install tools in WSL instance
 write-host "Installing tools inside the WSL distro..."
-Ubuntu1804 run apt install ansible -y
 Ubuntu1804 run apt install nodejs -y
 
+# Install VS
+choco install -y visualstudio2017community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
+Update-SessionEnvironment #refreshing env due to Git install
+
+#--- UWP Workload and installing Windows Template Studio ---
+choco install -y visualstudio2017-workload-azure
+choco install -y visualstudio2017-workload-universal
+choco install -y visualstudio2017-workload-manageddesktop
+choco install -y visualstudio2017-workload-nativedesktop
+
 # personalize
-choco install -y microsoft-teams
-choco install -y office365business
+## utils
+choco install -y conemu
+choco install -y ditto
+choco install -y greenshot
+choco install -y adobereader
+choco install -y vlc
+choco install -y youtube-dl
+choco install -y aria2
+choco install -y keepass.install
+choco install -y rufus
+
+## to create chocolatey packages
+choco install -y ussf
+choco install -y checksum
+
+## development
+choco install -y node
+choco install -y firacode 
+choco install -y postman
 
 # checkout recent projects
 mkdir C:\github
 cd C:\github
-git.exe clone https://github.com/microsoft/windows-dev-box-setup-scripts
-git.exe clone https://github.com/microsoft/winappdriver
-git.exe clone https://github.com/microsoft/wsl
-git.exe clone https://github.com/PowerShell/PowerShell
-
-# set desktop wallpaper
-Invoke-WebRequest -Uri 'http://chocolateyfest.com/wp-content/uploads/2018/05/img-bg-front-page-header-NO_logo-opt.jpg' -Method Get -ContentType image/jpeg -OutFile 'C:\github\chocofest.jpg'
-Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value 'C:\github\chocofest.jpg'
-rundll32.exe user32.dll, UpdatePerUserSystemParameters
-RefreshEnv
+git.exe clone https://github.com/ddieppa/windows-dev-box-setup-scripts.git
+git.exe clone https://github.com/ddieppa/colors.git
+git.exe clone https://github.com/ddieppa/ddieppa.github.io.git
+git.exe clone https://github.com/ddieppa/bitandbang.git
 
 Enable-UAC
 Enable-MicrosoftUpdate
